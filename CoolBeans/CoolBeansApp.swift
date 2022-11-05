42d100c18da8331e6914cc0a2bb78f1a74b54485
Ian Waddington <iwaddo@icloud.com>
2022-11-05 17:07:34 +0000

    Initial Commit

---
+++ CoolBeans/CoolBeansApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoolBeansApp.swift
+//  CoolBeans
+//
+//  Created by Ian Waddington on 05/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct CoolBeansApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
